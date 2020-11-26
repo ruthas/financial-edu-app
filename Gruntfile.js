c41828373d95057efae24941f8ccf50261484761
ruthas <62007410+ruthas@users.noreply.github.com>
2020-11-26 08:26:23 +0200

    Revert "grunt"
    This reverts commit d8701d25a425a246301a0c9c9831369a0cf39278.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,13 +8,13 @@ module.exports = function (grunt) {
       },
       build: {
         src: 'scripts/*.js',
-        dest: '<%= pkg.name %>.min.js'
+        dest: 'build/<%= pkg.name %>.min.js'
       }
     }
   })
 
   // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify', '')
+  grunt.loadNpmTasks('grunt-contrib-uglify')
 
   // Default task(s).
   grunt.registerTask('default', ['uglify'])
